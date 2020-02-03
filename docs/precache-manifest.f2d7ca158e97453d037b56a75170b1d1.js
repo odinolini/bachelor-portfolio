167a1ffbec57096c730c10d82878be5750159a98
odinolini <aoandberg@gmail.com>
2020-02-03 13:51:02 +0100

    docs folder for gh pages

---
+++ docs/precache-manifest.f2d7ca158e97453d037b56a75170b1d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cb750163cecf52a30cb0d59c062e4f",
+    "url": "/bachelor-portfolio/index.html"
+  },
+  {
+    "revision": "47b5a265d3ee5135b483",
+    "url": "/bachelor-portfolio/static/css/main.0ac54439.chunk.css"
+  },
+  {
+    "revision": "b5a05a49c6301788ab57",
+    "url": "/bachelor-portfolio/static/js/2.2e367778.chunk.js"
+  },
+  {
+    "revision": "47b5a265d3ee5135b483",
+    "url": "/bachelor-portfolio/static/js/main.67456894.chunk.js"
+  },
+  {
+    "revision": "446d05fb440ac3aa1a14",
+    "url": "/bachelor-portfolio/static/js/runtime-main.ae2df25b.js"
+  },
+  {
+    "revision": "5a9681c876cc158bf5e71eaa13bc162e",
+    "url": "/bachelor-portfolio/static/media/PDF.5a9681c8.svg"
+  },
+  {
+    "revision": "abaf5523824789117fd10766ab604fea",
+    "url": "/bachelor-portfolio/static/media/daniel.abaf5523.jpg"
+  },
+  {
+    "revision": "7f9ed6ca159f1bdf016b52267553d467",
+    "url": "/bachelor-portfolio/static/media/magnus.7f9ed6ca.jpg"
+  },
+  {
+    "revision": "76242bb7ff3f4da0a5650c310bab4786",
+    "url": "/bachelor-portfolio/static/media/odin.76242bb7.jpg"
+  },
+  {
+    "revision": "5d9bea127123e3dda25cc6484913b299",
+    "url": "/bachelor-portfolio/static/media/stian.5d9bea12.png"
+  }
+]);
